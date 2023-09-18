a894c6814e8ebff0080070d0a13721c5d0113e77
eddie <a589658gur@gmail.com>
2023-09-18 11:39:22 +0800

    step 1: The createElement Function

---
+++ scripts/React/index.js
@@ -0,0 +1,24 @@
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === "object" ? child : createElement(child))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+const Didact = {
+  createElement
+}
